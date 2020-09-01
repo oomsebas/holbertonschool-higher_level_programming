9a4ec8951686fd3abc0f5779dc8752691351fdf5
Sebastian_Orozco <omsebas@gmail.com>
2020-09-01 03:03:18 +0000

    eleventh task check circular linked list

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -6,6 +6,9 @@ int check_cycle(listint_t *list)
 	listint_t *head_value;
 	listint_t *current = NULL;
 
+	if (list == NULL | *list == '\0' )
+		return(0);
+
 	head_value = list;
 	current = list;
 	for (; current != NULL; current = current->next)
