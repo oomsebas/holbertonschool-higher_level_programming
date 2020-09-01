596dfa19330f3e14e082d9525e70c33886f63125
Sebastian_Orozco <omsebas@gmail.com>
2020-09-01 20:55:18 +0000

    Second target last digit

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -7,8 +7,10 @@ if number < 0:
 else:
     print_digit = last_digit
 if print_digit > 5 :
-    print("Last digit of {:d} is {:d} and is greater than 5".format(number, print_digit))
+    print("Last digit of {:d} is {:d} and is greater\
+    than 5".format(number, print_digit))
 elif print_digit  == 0:
     print("Last digit of {:d} is {:d} and is 0".format(number, print_digit))
 elif print_digit < 6 and print_digit is not 0:
-    print("Last digit of {:d} is {:d} and is less than 6 and not 0".format(number, print_digit))
+    print("Last digit of {:d} is {:d} and is less than\
+    6 and not 0".format(number, print_digit))
