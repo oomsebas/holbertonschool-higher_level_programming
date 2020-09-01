4b99826200e1858f38d55e72e13d1c345259715b
Sebastian_Orozco <omsebas@gmail.com>
2020-09-01 21:11:16 +0000

    Second task fix pep8 format

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -6,10 +6,10 @@ if number < 0:
     dig = last_digit * (-1)
 else:
     dig = last_digit
-str = "Last digit of "
+st = "Last digit of "
 if dig > 5:
-    print(str + "{:d} is {:d} and is greater than 5".format(number, dig))
+    print(st + "{:d} is {:d} and is greater than 5".format(number, dig))
 elif dig == 0:
-    print(str + "{:d} is {:d} and is 0".format(number, dig))
+    print(st + "{:d} is {:d} and is 0".format(number, dig))
 elif dig < 6 and dig is not 0:
-    print(str + "{:d} is {:d} and is less than 6 and not 0".format(number, dig))
+    print(st + "{:d} is {:d} and is less than 6 and not 0".format(number, dig))
