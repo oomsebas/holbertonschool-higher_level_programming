a04c4454ce3a8c04615156b4e91cce1a6febcce9
Sebastian_Orozco <omsebas@gmail.com>
2020-09-01 21:07:23 +0000

    Second task fix pep8 format

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,16 +1,15 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-last_digit = abs(number)%10
+last_digit = abs(number) % 10
 if number < 0:
-    print_digit = last_digit * (-1)
+    dig = last_digit * (-1)
 else:
-    print_digit = last_digit
-if print_digit > 5 :
-    print("Last digit of {:d} is {:d} and is greater\
-    than 5".format(number, print_digit))
-elif print_digit  == 0:
-    print("Last digit of {:d} is {:d} and is 0".format(number, print_digit))
-elif print_digit < 6 and print_digit is not 0:
-    print("Last digit of {:d} is {:d} and is less than\
-    6 and not 0".format(number, print_digit))
+    dig = last_digit
+str = "Last digit of "
+if dig > 5:
+    print(str + "{:d} is {:d} and is greater than 5".format(number, dig))
+elif dig == 0:
+    print(str + "{:d} is {:d} and is 0".format(number, dig))
+elif dig < 6 and dig is not 0:
+    print(str + "{:d} is {:d} and is less than 6 and not 0".format(number, dig))
