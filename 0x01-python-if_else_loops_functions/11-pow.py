7f6434418400e8458b8522d06b53bf60c03a8434
Sebastian_Orozco <omsebas@gmail.com>
2020-09-02 02:05:54 +0000

    Twelve task

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,14 +1,3 @@
 #!/usr/bin/python3
 def pow(a, b):
-    c = 1
-    if (b == 0):
-        return 1
-    if (a == 0 and b < 0):
-        return
-    if b > 0:
-        for i in range(b):
-            c = c * a
-    else:
-        for i in range(abs(b)):
-            c = c / a
-    return c
+    return (a ** b)
