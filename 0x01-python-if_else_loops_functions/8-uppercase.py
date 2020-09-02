601ebe96c7c59f2e895f73270685f1c52ffe5057
Sebastian_Orozco <omsebas@gmail.com>
2020-09-02 01:02:53 +0000

    nineth task

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for letter in str:
+        let = ord(letter)
+        if ord(letter) >= 97 and ord(letter) <= 122:
+            let = let -32
+        print("{}".format(chr(let)), end="")
+    print("")
