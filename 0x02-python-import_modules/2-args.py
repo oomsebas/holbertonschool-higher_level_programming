509b62db81101d858b8e43f813cd43bf4d7af5c7
Sebastian_Orozco <omsebas@gmail.com>
2020-09-04 01:19:03 +0000

    Third task

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+from sys import argv
+
+if __name__ == '__main__':
+
+    if len(argv) == 1:
+        print("0 arguments.")
+
+    else:
+
+        print("{} arguments:".format(len(argv) - 1))
+
+        for index, args in enumerate(argv):
+            if index >= 1:
+                print("{}: {}".format(index, argv[index]))
