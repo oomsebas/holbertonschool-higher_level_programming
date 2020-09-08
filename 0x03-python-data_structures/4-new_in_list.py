cdcf026527fbd9f10758b26da044ab8074b4513b
Sebastian_Orozco <omsebas@gmail.com>
2020-09-08 22:28:59 +0000

    Task 4 replace in copy

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
     cpy_list = []
-    lent = len(my_list)
 
+    lent = len(my_list)
     cpy_list = my_list.copy()
+    if my_list is None and element is none:
+        return (cpy_list)
     if idx < 0 or idx > lent:
         return (cpy_list)
     else:
