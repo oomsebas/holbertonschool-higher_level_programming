c943f8ab217a9a3b1dc0dcb34c0ba5d853dee69c
Sebastian_Orozco <omsebas@gmail.com>
2020-09-09 05:13:51 +0000

    Task 13 detect if a linked list is a palindrome

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -31,3 +31,26 @@ int is_palindrome(listint_t **head)
 	}
 	return (1);
 }
+
+/**
+ *add_nodeint - function that creates a node at the beginning
+ *@head: pointer to the first node;
+ *@n: int to add to the new node;
+ *Return: pointer to the first element of the list;
+ */
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *new;
+
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->n = n;
+	new->next = *head;
+	*head = new;
+
+	return (*head);
+
+}
+
