2fc447cea729817d626acaf82be74df752f4c9c8
Sebastian_Orozco <omsebas@gmail.com>
2020-09-09 02:11:09 +0000

    Task 7 add two items tupple

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    tup = tuple((tuple_a, tuple_b))
-    tup1 = 0
-    tup2 = 0
+    if (tuple_a is None) or (tuple_b is None):
+        return
 
-    for enum, i in enumerate(tup):
-        for enum1, j in enumerate(i):
-            if enum1 == 0:
-                tup1 = tup1 + j
-            else:
-                tup2 = tup2 + j
+    if (len(tuple_a) == 1):
+        tuple_a = tuple_a[0], 0
+    elif (len(tuple_a) == 0):
+        tuple_a = 0, 0
 
-    return (tup1, tup2)
+    if (len(tuple_b) == 1):
+        tuple_b = tuple_b[0], 0
+    elif (len(tuple_b) == 0):
+        tuple_b = 0, 0
+
+    return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
