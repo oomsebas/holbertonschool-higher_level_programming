b55ab859e9424d653ff3d19ddd2aa04ebd5b1a03
Sebastian_Orozco <omsebas@gmail.com>
2020-09-09 03:45:29 +0000

    Task 11 delete item from a list

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
 
-    if (my_list is None) and (len(my_list)) < 1:
-        return None
+    if (my_list is None) and (idx is None):
+        return my_list
 
-    if (idx < 0) and (idx > len(my_list)):
+    if (idx < 0) and (idx >= len(my_list)):
         return (my_list)
 
     del my_list[idx]
