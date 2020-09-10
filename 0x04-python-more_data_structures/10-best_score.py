90ef2f39f59ac1a6cab6747b6967bc59d97ea894
Sebastian_Orozco <omsebas@gmail.com>
2020-09-10 22:05:46 +0000

    Task 10 best score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     a = None
-    if a_dictionary is None:
+    if a_dictionary:
         a = max(a_dictionary, key=a_dictionary.get)
     return a
