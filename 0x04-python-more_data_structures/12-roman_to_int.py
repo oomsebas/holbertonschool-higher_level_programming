820cc9c220987b03bccca28133dedde2fd03b6be
Sebastian_Orozco <omsebas@gmail.com>
2020-09-11 00:22:13 +0000

    Task 12 roman to decimal

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,13 @@
+#!/usr/bin/pyhton3
+def roman_to_int(roman_string):
+    dec = []
+    sum = 0
+    rom = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
+    dec = [rom.get(i) for i in roman_string]
+    for i, j in zip(dec, dec[1:]):
+        if i >= j:
+            sum = sum + i
+        else:
+            sum = sum - i
+    sum = sum + dec[-1]
+    return sum
