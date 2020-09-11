de99dd06913af33e54864e1df899cb68c3d0f358
Sebastian_Orozco <omsebas@gmail.com>
2020-09-11 00:31:48 +0000

    Task 12 roman to decimal

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
     dec = []
+    if type(roman_string is not str) | (roman_string is None):
+        return 0
     sum = 0
     rom = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
     dec = [rom.get(i) for i in roman_string]
