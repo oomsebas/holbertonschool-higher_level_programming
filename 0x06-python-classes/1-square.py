9e7cf5185997f47846011337299f557a324c356c
Sebastian_Orozco <omsebas@gmail.com>
2020-09-22 23:45:49 +0000

    Task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Empty class Square: defines the square of a number"""
+
+
+class Square:
+    """initialization method with class variable size"""
+    def __init__(self, size):
+        self.__size = size
+    pass
