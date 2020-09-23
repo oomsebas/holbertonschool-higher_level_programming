f04ac97ca77b2e815c1532cd750854a2778af80a
Sebastian_Orozco <omsebas@gmail.com>
2020-09-23 00:30:15 +0000

    Task 2

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Empty class Square: defines the square of a number
+"""
+
+
+class Square:
+    """initialization method with class variable size"""
+    def __init__(self, size=0):
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+    pass
