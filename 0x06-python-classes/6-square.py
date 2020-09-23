f964ade95518e9be201c40aad7351a3f32114eda
Sebastian_Orozco <omsebas@gmail.com>
2020-09-23 03:47:51 +0000

    Task 6

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,79 @@
+#!/usr/bin/python3
+"""Empty class Square: defines the square of a number
+"""
+
+
+class Square:
+    """initialization method with  private class variable size"""
+    def __init__(self, size=0, position=(0, 0)):
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+        if type(position[0]) is not int and type(position[1]) is not int:
+            TypeError("position must be a tuple of 2 positive integers")
+        elif position[0] < 0 or position[1] < 0:
+            TypeError("position must be a tuple of 2 positive integers")
+        elif len(position) is not 2:
+            TypeError("position must be a tuple of 2 positive integers")
+        else:
+            self.__position = position
+
+    def area(self):
+        """ method that squares the size"""
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        """Get the size class variable"""
+        return self.__size * 1
+
+    @size.setter
+    def size(self, value):
+        """Set a private class variable"""
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def my_print(self):
+        """prints in stdout the square with the character #"""
+        if self.__size == 0:
+            print()
+            return
+
+        left_pos = self.__position[0]
+        print("\n" * self.__position[1], end="")
+        for i in range(self.__size):
+            for j in range(self.__size + self.__position[0]):
+                if left_pos > 0 and self.__position[1] == 0:
+                    print(" ", end="")
+                    left_pos -= 1
+                elif left_pos > 0 and self.__position[1] > 0:
+                    print("_", end="")
+                    left_pos -= 1
+                else:
+                    print("#", end="")
+                left_pos = self.position[0]
+                print()
+
+    @property
+    def position(self):
+        """Getter for position variable"""
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        """Setter for position private instance attribute"""
+        if type(position[0]) is not int and type(position[1]) is not int:
+            TypeError("position must be a tuple of 2 positive integers")
+        elif position[0] < 0 or position[1] < 0:
+            TypeError("position must be a tuple of 2 positive integers")
+        elif len(position) is not 2 and type(position) is not tuple:
+            TypeError("position must be a tuple of 2 positive integers")
+        else:
+            self.__position = position
