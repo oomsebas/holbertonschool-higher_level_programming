4a874c3362480b0ce5b69b6567e99d455eb3fbe9
Sebastian_Orozco <omsebas@gmail.com>
2020-09-26 05:36:28 +0000

    Change on some test to evaluate floats

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -8,28 +8,45 @@ max_integer = __import__('6-max_integer').max_integer
 
 class TestMaxInteger(unittest.TestCase):
 
-    def regular_cases(self):
-        """Check for regular inputs"""
-        self.assertEqual(max_integer([1, 2, 3, 4, 5]), 5)
-        self.assertEqual(max_integer([50, 100, 1, -8]), 100)
-        self.assertEqual(max_integer([-1, -10, -3]), -1)
+    def test_max(self):
+        """Check output for normal input"""
+        self.assertEqual(max_integer([1, 50, 2]), 50)
+        self.assertEqual(max_integer([-5, -10, -9]), -5)
+        self.assertEqual(max_integer([1]), 1)
+        self.assertEqual(max_integer([1, 1, 1, 1]), 1)
+        self.assertEqual(max_integer([2, 4] * 3), 4)
 
-    def check_empty(self):
+    def test_empty(self):
         """Check for an empty list"""
-        self.assertIsNone(max_integer([]))
+        self.assertEqual(max_integer([]), None)
 
-    def check_tuple(self):
+    def test_tuple(self):
         """Check for different types of input"""
-        self.assertEqual(max_integer((2, 3)), 3)
+        with self.assertRaises(TypeError):
+            _max = max_integer((2, 3))
 
-    def check_None(self):
+    def test_None(self):
         """Check for different types of input"""
-        self.assertIsNone(max_integer(None))
+        with self.assertRaises(TypeError):
+            _max = max_integer(None)
+
+    def test_big_number(self):
+        """Check for big numbers"""
+        _list = [2135456413213546541321324163463123132746,
+                 13524654351321324135461321354]
+        self.assertEqual(max_integer(_list),
+                         2135456413213546541321324163463123132746)
 
     def test_float(self):
         """Check for floats"""
-        _list = [1.5, 2.4, 3.5, 4.1]
-        self.assertEqual(max_integer(_list), 4.1)
+        _list = [2.3, 5.6, 7.8]
+        with self.assertRaises(TypeError):
+            _max = max_integer(_list)
+
+    def test_inf(self):
+        _list = [float('inf'), float('inf')]
+        with self.assertRaises(TypeError):
+            _max = max_integer(_list)
 
     def test_docstringmod(self):
         """Check docstring for module"""
@@ -41,7 +58,6 @@ class TestMaxInteger(unittest.TestCase):
         self.assertTrue(f is not None and len(f) > 5)
 
     def test_morethan(self):
-        # check that max_integer fails when you pass more than one argument
         with self.assertRaises(TypeError):
             _max = max_integer(1, 1)
 
