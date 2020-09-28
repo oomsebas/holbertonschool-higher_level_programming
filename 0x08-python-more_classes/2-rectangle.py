6bf95e78f6e9ba13057e4c11226c11fb46e92a04
Sebastian_Orozco <omsebas@gmail.com>
2020-09-28 23:34:54 +0000

    Fix test conditions

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -51,4 +51,6 @@ class Rectangle:
 
     def perimeter(self):
         """Method that returns the perimeter of the rectangle"""
+        if self.__width is 0 or self.__height is 0:
+            return 0
         return (2 * self.__width) + (2 *self.__height)
