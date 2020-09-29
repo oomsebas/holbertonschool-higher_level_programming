82f9b163a91fbc8e155f95a3aba93d77638f9071
Sebastian_Orozco <omsebas@gmail.com>
2020-09-29 00:33:06 +0000

    Task 3 fix error messages and code form  the last task

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -1,4 +1,4 @@
-#!/usr/bin/pyhton3
+#!/usr/bin/python3
 """ Create a empty class for a rectangle"""
 
 
@@ -6,7 +6,15 @@ class Rectangle:
     """ Class rectangle"""
     def __init__(self, width=0, height=0):
         """init method"""
+        if type(width) is not int:
+            raise TypeError('width must be an integer')
+        elif width < 0:
+            raise ValueError('width must be >= 0')
         self.__width = width
+        if type(height) is not int:
+            raise TypeError('height must be an integer')
+        elif height < 0:
+            raise ValueError('height must be >= 0')
         self.__height = height
 
     @property
@@ -18,9 +26,9 @@ class Rectangle:
     def width(self, value):
         """width setter"""
         if type(value) is not int:
-            raise TypeError('height must be an integer')
+            raise TypeError('width must be an integer')
         elif value < 0:
-            raise ValueError('height must be >= 0')
+            raise ValueError('width must be >= 0')
         self.__width = value
 
     @property
@@ -43,7 +51,9 @@ class Rectangle:
 
     def perimeter(self):
         """Method that returns the perimeter of the rectangle"""
-        return 2 * (self.__width + self.__height)
+        if self.__width is 0 or self.__height is 0:
+            return 0
+        return (2 * self.__width) + (2 * self.__height)
 
     def __str__(self):
         square = ""
