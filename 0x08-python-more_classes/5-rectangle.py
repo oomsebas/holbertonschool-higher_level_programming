1eae4eef5c4b3342de218dd7afd098ccb5742458
Sebastian_Orozco <omsebas@gmail.com>
2020-09-29 01:05:00 +0000

    Task 5 deletion of an object detection

--- 0x08-python-more_classes/5-rectangle.py
+++ 0x08-python-more_classes/5-rectangle.py
@@ -70,4 +70,4 @@ class Rectangle:
         return "Rectangle(" + str(self.width) + ", " + str(self.height) + ")"
 
     def __del__(self):
-        print "Bye rectangle ..."
+        print("Bye rectangle ...")
