86b0859d8f0b0e82cbc1ed827a0c042e7416d903
Sebastian_Orozco <omsebas@gmail.com>
2020-09-29 00:55:03 +0000

    Task 4 using __repr__ to create a representation object

---
+++ 0x08-python-more_classes/4-rectangle.py
@@ -0,0 +1,70 @@
+#!/usr/bin/python3
+""" Create a empty class for a rectangle"""
+
+
+class Rectangle:
+    """ Class rectangle"""
+    def __init__(self, width=0, height=0):
+        """init method"""
+        if type(width) is not int:
+            raise TypeError('width must be an integer')
+        elif width < 0:
+            raise ValueError('width must be >= 0')
+        self.__width = width
+        if type(height) is not int:
+            raise TypeError('height must be an integer')
+        elif height < 0:
+            raise ValueError('height must be >= 0')
+        self.__height = height
+
+    @property
+    def width(self):
+        """ width getter"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """width setter"""
+        if type(value) is not int:
+            raise TypeError('width must be an integer')
+        elif value < 0:
+            raise ValueError('width must be >= 0')
+        self.__width = value
+
+    @property
+    def height(self):
+        """ height getter"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """ height setter"""
+        if type(value) is not int:
+            raise TypeError('height must be an integer')
+        elif value < 0:
+            raise ValueError('height must be >= 0')
+        self.__height = value
+
+    def area(self):
+        """Method that returns the area of the rectangle"""
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """Method that returns the perimeter of the rectangle"""
+        if self.__width is 0 or self.__height is 0:
+            return 0
+        return (2 * self.__width) + (2 * self.__height)
+
+    def __str__(self):
+        """ print square"""
+        square = ""
+        if self.__width is 0 or self.__height is 0:
+            return ""
+        for i in range(self.__height - 1):
+            square += (self.__width * "#") + "\n"
+        square += (self.__width * "#")
+        return square
+
+    def __repr__(self):
+        """method to create a new obkectec using eval"""
+        return "Rectangle(" + str(self.width) + ", " + str(self.height) + ")"
