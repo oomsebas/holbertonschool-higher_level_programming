ce9689cd2d6ca2d14e69572398a7c2bb9cc34446
Sebastian_Orozco <omsebas@gmail.com>
2020-10-06 23:30:57 +0000

    Task 1

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Task 1, write a class myList that inherits from lists"""
+
+class MyList(list):
+    """Definition of a class Mylist which inherits from list"""
+
+    def print_sorted(self):
+        """ function that prints a sorted list"""
+        print(sorted(self))
