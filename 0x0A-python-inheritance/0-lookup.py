534d15f12d885dd2b7a6fdba61407a0f0d421c58
Sebastian_Orozco <omsebas@gmail.com>
2020-10-06 21:21:50 +0000

    Task 0,print the list of methods and attributes of an object

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,7 @@
+#!/usr/bin/pyhton3
+"""Task 0 print the list of methods and attributes"""
+
+
+def lookup(obj):
+    """functions that returns the list available of methos and attributes"""
+    return list(dir(obj))
