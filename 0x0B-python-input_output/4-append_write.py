c1acc30f57eacc4b2c0b7d41f9993abbf08f15cc
Sebastian_Orozco <omsebas@gmail.com>
2020-10-07 21:19:47 +0000

    Task 4 append to a file

---
+++ 0x0B-python-input_output/4-append_write.py
@@ -0,0 +1,10 @@
+#!/usr/bin/pyhton3
+""" Task 4 append to a file"""
+
+
+def append_write(filename="", text=""):
+    """fiuction that appends to a file"""
+    with open(filename, "a", encoding="utf-8") as _file:
+        num_lines = 0
+        num_lines = _file.write(text)
+        return num_lines
