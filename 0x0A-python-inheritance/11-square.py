8a94d066047fb6af380397f2f3466e8c1fd9081f
Sebastian_Orozco <omsebas@gmail.com>
2020-10-07 03:30:33 +0000

    Task 11

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Task 11 create a parent class"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """ Square class
+    """
+    def __init__(self, size):
+        """constructor of Square"""
+        self.integer_validator('size', size)
+        self.__size = size
+        super().__init__(size, size)
