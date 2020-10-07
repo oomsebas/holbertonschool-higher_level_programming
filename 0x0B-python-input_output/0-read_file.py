f4c66620efb5e734b897714641d3e3157863175d
Sebastian_Orozco <omsebas@gmail.com>
2020-10-07 21:55:08 +0000

    Task 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,5 +4,6 @@
 
 def read_file(filename=""):
     """Function that reads a file"""
-    with open(filename, encoding="UTF-8") as Myfile:
-        print(Myfile.read(), end='')
+    with open(filename, encoding="utf-8") as Myfile:
+        for line in Myfile:
+            print(line, end='')
