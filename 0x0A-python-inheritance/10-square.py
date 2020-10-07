1ccfafe7b59cd9513ef66a0cbc7b68f561087ca6
Sebastian_Orozco <omsebas@gmail.com>
2020-10-07 03:22:23 +0000

    Task 10

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -2,26 +2,7 @@
 """Task 8 create a parent class"""
 
 
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
-
-
-class Rectangle(BaseGeometry):
-    """class rectangle
-    """
-    def __init__(self, width, height):
-        """init class"""
-        self.integer_validator('width', width)
-        self.integer_validator('height', height)
-        self.__width = width
-        self.__height = height
-        super().__init__()
-
-    def area(self):
-        """ empty class"""
-        return self.__width * self.__height
-
-    def __str__(self):
-        return ("[Rectangle] " + str(self.__width) + "/" + str(self.__height))
+Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
