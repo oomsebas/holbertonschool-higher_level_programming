b563baf029a2705a2d2ef1775d84696660bae587
Sebastian_Orozco <omsebas@gmail.com>
2020-10-07 00:10:46 +0000

    Task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Task 6 create a parent class"""
+
+
+class BaseGeometry():
+    """create a parent class"""
+    def area(self):
+        raise Exception("area() is not implemented")
