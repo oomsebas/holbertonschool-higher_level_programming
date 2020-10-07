17f18925b6fb96747d9d97292bf26b7f7278c81c
Sebastian_Orozco <omsebas@gmail.com>
2020-10-07 02:13:10 +0000

    change in the comments

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
-"""Task 6 create a parent class"""
+"""Task 7 create a parent class"""
 
 
 class BaseGeometry():
-    """create a parent class"""
+    """create a parent class
+    """
     def area(self):
+        """ empty class"""
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
