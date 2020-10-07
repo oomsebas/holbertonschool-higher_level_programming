1f2b1caf1c9d63630fdb8d628d836520402b788b
Sebastian_Orozco <omsebas@gmail.com>
2020-10-07 00:21:26 +0000

    Task 7

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Task 6 create a parent class"""
+
+
+class BaseGeometry():
+    """create a parent class"""
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """ integer validator return error if value es not an integer
+
+        Args:
+        name(string): name of the variable
+        value(int): value to check
+        """
+        if type(value) is not int:
+            raise TypeError(name + " must be an integer")
+        elif value <= 0:
+            raise ValueError(name + " must be greater than 0")
