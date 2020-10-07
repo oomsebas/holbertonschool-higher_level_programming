db97ac870885c2d6f4d4302c0a13f4b0631f983a
Sebastian_Orozco <omsebas@gmail.com>
2020-10-07 19:04:25 +0000

    Task 1

---
+++ 0x0B-python-input_output/1-number_of_lines.py
@@ -0,0 +1,12 @@
+#!/usr/bin/pyhton3
+"""Task 1 count the number of lines of a file"""
+
+
+def number_of_lines(filename=""):
+    """Function to count lines in a file"""
+    with open(filename, encoding="utf-8") as _file:
+        lines = 0
+        while _file.readline() != '':
+            lines += 1
+
+        return(lines)
