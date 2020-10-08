e5a6f9b3458218c12ff065f8c13b4f259449b97c
Sebastian_Orozco <omsebas@gmail.com>
2020-10-08 01:03:37 +0000

    Task 9

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
 """read and write a json object"""
+
+
 from sys import argv
 import json
 import os.path
+
+
 save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
 
