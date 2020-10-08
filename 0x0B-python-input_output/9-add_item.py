5b0cdd96730e89e20f3886774ce983b252783bd3
Sebastian_Orozco <omsebas@gmail.com>
2020-10-08 01:02:35 +0000

    Task 9

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -2,19 +2,14 @@
 """read and write a json object"""
 from sys import argv
 import json
+import os.path
 save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
 
-_list = []
-try:
-    _list = load_from_json_file("additem.json")
-
-except FileNotFoundError:
-    save_to_json_file(_list, "additem.json")
-
-finally:
-    _list = load_from_json_file("additem.json")
-    for item in range(1, len(argv)):
-        _list.append(argv[item])
-
-        save_to_json_file(_list, "additem.json")
+filename = "add_item.json"
+if os.path.isfile(filename):
+    _list = load_from_json_file(filename)
+else:
+    _list = []
+_list.extend(argv[1:])
+save_to_json_file(_list, "additem.json")
