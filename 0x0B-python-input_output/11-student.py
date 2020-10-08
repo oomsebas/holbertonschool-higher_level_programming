151b6310f0d88a445edbce8636dac2589c951e11
Sebastian_Orozco <omsebas@gmail.com>
2020-10-08 02:31:44 +0000

    task 11

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -1,14 +1,17 @@
-#!/usr/bin/pyhton3
+#!/usr/bin/python3
 """task 11 create a class"""
 
 
 class Student:
     """ class student
     """
+
     def __init__(self, first_name, second_name, age):
+        """init method"""
         self.first_name = first_name
         self.second_name = second_name
         self.age = age
 
     def to_json(self):
+        """to json"""
         return self.__dict__
