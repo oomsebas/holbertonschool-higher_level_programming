7562c2f58cf0688d1d5f7d94d3f931a9af8fffea
Sebastian_Orozco <omsebas@gmail.com>
2020-10-08 03:22:26 +0000

    Task 12

---
+++ 0x0B-python-input_output/12-student.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""task 11 create a class"""
+
+
+class Student:
+    def __init__(self, first_name, last_name, age):
+        """init method"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """to json"""
+        dict = {}
+        if attrs is None and type(attrs) is not list:
+            return self.__dict__
+        if all(isinstance(s, str) for s in attrs):
+            for inst in attrs:
+                dict[inst] = self.__dict__.get(inst)
+            return dict
+        else:
+            return self.__dict__
