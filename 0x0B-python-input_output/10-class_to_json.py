f0f9df6120d85e5bbde856cfc03a4f4f7c6ed426
Sebastian_Orozco <omsebas@gmail.com>
2020-10-08 01:50:06 +0000

    Task 10

--- 0x0B-python-input_output/10-class_to_json.py
+++ 0x0B-python-input_output/10-class_to_json.py
@@ -5,9 +5,6 @@
 import json
 
 
-to_json_string = __import__('5-to_json_string').to_json_string
-
-
 def class_to_json(obj):
     """function that turn objet dict to json"""
-    return (to_json_string(obj.__dict__))
+    return (json.dumps(obj.__dict__))
