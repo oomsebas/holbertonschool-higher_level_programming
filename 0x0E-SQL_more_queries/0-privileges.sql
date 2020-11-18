5acc298cb01ad5daa31a41d0f69926b2d5313498
Sebastian_Orozco <omsebas@gmail.com>
2020-11-18 00:41:16 +0000

    Task 0

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,5 +1,5 @@
 -- Write a script that lists all privileges of the MySQL users.
 -- show grants
 
-SHOW GRANTS FOR user_0d_1@localhost;
-SHOW GRANTS FOR user_0d_2@localhost;
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR ' user_0d_2'@'localhost';
