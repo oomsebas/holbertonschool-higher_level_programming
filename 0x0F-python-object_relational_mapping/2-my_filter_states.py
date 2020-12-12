2b6b2e1af46dcbfd33e3f7b76c413ec8baf3bba0
Sebastian_Orozco <omsebas@gmail.com>
2020-12-12 23:14:23 +0000

    task 2

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" Write a script that lists all states from the database hbtn_0e_0_usa """
+import MySQLdb
+from sys import argv
+
+
+def get_all_states(username, password, dbname, stname):
+    """function that returns all the states from a database
+    Your script should take 4 arguments: mysql username, mysql password,
+    database name and state name searched"""
+    db = MySQLdb.connect(host="localhost", port=3306, user=str(username),
+                         passwd=str(password), db=str(dbname), charset="utf8")
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY '" + stname + "';")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
+
+if __name__ == '__main__':
+    get_all_states(argv[1], argv[2], argv[3], argv[4])
