e0e9cdae481afcd9167fd3a91693ae4d193bae1b
Sebastian_Orozco <omsebas@gmail.com>
2021-01-11 21:37:56 +0000

    Task 1

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -2,9 +2,9 @@
 """
  requests header content to a page and retrives a variable
 """
-if __name__ == "__main__" :
-    import urllib.request as request
-    from sys import argv
+import urllib.request as request
+from sys import argv
+if __name__ == "__main__":
     req = request.Request(argv[1])
     with request.urlopen(req) as response:
         print(response.headers.get("X-Request-Id"))
