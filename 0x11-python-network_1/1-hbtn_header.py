d8c5b52fb71e76f3bc3e4fb1640358efc0ea48da
Sebastian_Orozco <omsebas@gmail.com>
2021-01-11 21:13:05 +0000

    Task 1

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-""" request content to a pagae """
+""" request header content to a page """
 import urllib.request
-import sys
-req = urllib.request.Request(sys.argv[1])
+from sys import argv
+req = urllib.request.Request(argv[1])
 with urllib.request.urlopen(req) as response:
     html2 = response.headers
 res = html2.get("X-Request-Id")
