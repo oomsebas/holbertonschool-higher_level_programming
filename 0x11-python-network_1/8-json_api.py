b75f52493c9f6f83f01196f89a2661b18abad48d
Sebastian_Orozco <omsebas@gmail.com>
2021-01-12 01:23:10 +0000

    Task8

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -3,13 +3,13 @@
 import requests
 from sys import argv
 if __name__ == '__main__':
-        if len(argv) is 1 or len(argv[1]) is 0:
-                dict1 = {'q': ""}
+        if len(argv) <= 1 or len(argv[1]) is 0:
+                dict1 = {'q': ''}
         else:
                 dict1 = {'q': argv[1][0]}
         url = 'http://f29444c84915.87bf5168.hbtn-cod.io:5000/search_user'
         url1 = 'http://0.0.0.0:5000/search_user'
-        r = requests.post(url1, data=dict1)
+        r = requests.post(url, data=dict1)
         try:
                 text = r.json()
         except:
