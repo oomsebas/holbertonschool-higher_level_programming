305fc30f641a2bca4f38415e603d55bf208f16da
Sebastian_Orozco <omsebas@gmail.com>
2021-01-12 02:07:25 +0000

    Task8

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -6,17 +6,17 @@ if __name__ == '__main__':
         if len(argv) <= 1 or len(argv[1]) is 0:
                 dict1 = {'q': ''}
         else:
-                dict1 = {'q': argv[1][0]}
+                dict1 = {'q': argv[1]}
         url = 'http://f29444c84915.87bf5168.hbtn-cod.io:5000/search_user'
         url1 = 'http://0.0.0.0:5000/search_user'
         r = requests.post(url1, data=dict1)
         try:
                 text = r.json()
-        except:
-                print("Not a valid JSON")
-        else:
-                if len(text) is 0 or 'id' not in text.keys() or 'name' not in text.keys():
+                id = text.get('id')
+                name = text.get('name')
+                if len(text) is 0 or not id or not name:
                         print("Not result")
                 else:
-                        print("[{}] {}".format(text.get('id'),
-                                               text.get('name')))
+                        print("[{}] {}".format(id, name))
+        except ValueError:
+                print("Not a valid JSON")
