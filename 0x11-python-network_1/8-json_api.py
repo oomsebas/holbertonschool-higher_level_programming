584f7adede1aa947182deb36132967a337a41ed7
Sebastian_Orozco <omsebas@gmail.com>
2021-01-12 01:12:39 +0000

    Task8

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -6,7 +6,7 @@ if __name__ == '__main__':
         if len(argv) is 1:
                 dict1 = {'q': ""}
         else:
-                dict1 = {'q': argv[1]}
+                dict1 = {'q': argv[1][0]}
         url = 'http://f29444c84915.87bf5168.hbtn-cod.io:5000/search_user'
         url1 = 'http://0.0.0.0:5000/search_user'
         r = requests.post(url1, data=dict1)
