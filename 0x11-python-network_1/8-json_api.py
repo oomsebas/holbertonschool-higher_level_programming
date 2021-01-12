c5d257221b1c595e398b34fdb30062281ff08d78
Sebastian_Orozco <omsebas@gmail.com>
2021-01-12 02:37:15 +0000

    Task8

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,21 +1,21 @@
 #!/usr/bin/python3
-""" Creates a request to an API """
+""" Creates a request to an API  to an url page and return a json"""
 if __name__ == '__main__':
-        import requests
-        from sys import argv
-        if len(argv) == 2:
-            q = argv[1]
+    import requests
+    from sys import argv
+    if len(argv) == 2:
+        q = argv[1]
+    else:
+        q = ""
+    url1 = 'http://0.0.0.0:5000/search_user'
+    r = requests.post(url1, data={'q': q})
+    try:
+        text = r.json()
+        id = text.get('id')
+        name = text.get('name')
+        if len(text) == 0 or not id or not name:
+            print("Not result")
         else:
-            q = ""
-        url1 = 'http://0.0.0.0:5000/search_user'
-        r = requests.post(url1, data={'q': q})
-        try:
-            text = r.json()
-            id = text.get('id')
-            name = text.get('name')
-            if len(text) == 0 or not id or not name:
-                print("Not result")
-            else:
-                print("[{}] {}".format(id, name))
-        except:
-                print("Not a valid JSON")
+            print("[{}] {}".format(id, name))
+    except:
+        print("Not a valid JSON")
