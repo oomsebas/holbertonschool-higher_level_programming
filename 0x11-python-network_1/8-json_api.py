b1a10651ff95a26a044945fdf28d58203f0a6222
Sebastian_Orozco <omsebas@gmail.com>
2021-01-12 00:51:01 +0000

    Task8

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" Creates a request with resques package """
+import requests
+from sys import argv
+if __name__ == '__main__':
+        if len(argv) is 1:
+                dict1 = {'q': ""}
+        else:
+                dict1 = {'q': argv[1]}
+        url = 'http://f29444c84915.87bf5168.hbtn-cod.io:5000/search_user'
+        r = requests.post(url, data=dict1)
+        try:
+                text = r.json()
+                if len(text) is 0:
+                        print("Not result")
+                else:
+                        print("[{}] {}".format(text.get('id'),
+                                               text.get('name')))
+        except ValueError:
+                print("Not a valid JSON")
