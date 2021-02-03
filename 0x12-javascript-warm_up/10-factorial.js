f54024fa562047d880f0d48f8d6651d435d10b9b
Sebastian_Orozco <omsebas@gmail.com>
2021-02-03 02:36:49 +0000

    task 11

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -2,13 +2,29 @@
 // Write a script that computes and prints a factorial
 
 const fact = Number(process.argv.slice(2)[0]);
-let mult = 1;
 
-if (!fact) {
-  console.log('1');
-} else {
-  for (let i = 1; i <= fact; i++) {
-    mult = mult * i;
+function factorial (a) {
+  const mult = 1;
+  const i = 1;
+  let res = 0;
+  if (!a) {
+    console.log('1');
+  } else {
+    res = acum(mult, i, a);
+    console.log(res);
   }
-  console.log(mult);
 }
+
+function acum (mult, i, a) {
+  let res = 0;
+
+  mult *= i;
+  i = i + 1;
+  if (i > a) {
+    return (mult);
+  }
+  res = acum(mult, i, a);
+  return (res);
+}
+
+factorial(fact);
