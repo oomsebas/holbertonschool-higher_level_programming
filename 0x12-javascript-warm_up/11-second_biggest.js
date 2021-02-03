b0586b543ba19c4c2c1e9edbd4e788b89ef20f49
Sebastian_Orozco <omsebas@gmail.com>
2021-02-03 04:21:22 +0000

    Task 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,16 +1,10 @@
 #!/usr/bin/node
 // Write a script that searches the second biggest integer in the list of arguments.
-let lst = process.argv.slice(2);
+const lst = process.argv.slice(2);
 if (lst.length === 0 || (lst.length === 1)) {
   console.log(0);
 } else {
-  lst = lst.map(Number);
-  for (let i = 0; i < lst.length; i++) {
-    if (lst[i + 1] < lst[i]) {
-      const temp = lst[i + 1];
-      lst[i + 1] = lst[i];
-      lst[i] = temp;
-    }
-  }
-  console.log(lst[lst.length - 2]);
+  lst.sort(function (a, b) { return a - b; });
+  lst.reverse();
+  console.log(lst[1]);
 }
